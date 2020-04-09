563c87646a310af60d9551a1a2d3037c1faca9bd
Miguel Rivera <miguel.sky.215@gmail.com>
2020-04-09 13:08:37 -0400

    Set JWT auth endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :api do
+    resources :users
+    post 'token' => 'user_token#create'
+  end
 end
